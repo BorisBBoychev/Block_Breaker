42ad244836641e0c2ea2c0941cc1d3b5119ef01f
BorisBBoychev <borisboychev007@Gmail.com>
2020-04-19 19:29:11 +0300

    block colors and destruction

---
+++ Assets/Scripts/BlockCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class BlockCollision : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ColorChanger : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    float timeLeft;
+    public Color newColor;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void ChangerOfColors()
+    {
+        if (spriteRenderer != null)
+        {
+            if (timeLeft <= Time.deltaTime)
+            {
+                spriteRenderer.material.color = newColor;
+
+                newColor = new Color(Random.value, Random.value, Random.value);
+                timeLeft = 1.0f;
+            }
+            else
+            {
+                spriteRenderer.material.color =
+                    Color.Lerp(spriteRenderer.material.color, newColor, Time.deltaTime / timeLeft);
+
+                timeLeft -= Time.deltaTime;
+            }
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        ChangerOfColors();
+    }
+
+}
